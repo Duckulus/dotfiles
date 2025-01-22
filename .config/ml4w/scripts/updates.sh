55f5364004fc2f8f99c0d17484b2e269e830b49c
Duckulus <amin.haddou@outlook.de>
2025-01-22 16:29:11 +0100

    fix powermenu

--- .config/ml4w/scripts/updates.sh
+++ .config/ml4w/scripts/updates.sh
@@ -27,7 +27,7 @@ case $install_platform in
 
         # flatpak remote-ls --updates
 
-        updates=$(checkupdates-with-aur | wc -l)
+        updates=$(CHECKUPDATES_DB="/tmp/checkup-db-${UID}-$$" checkupdates-with-aur | wc -l)
     ;;
     fedora)
         updates=$(dnf check-update -q|grep -c ^[a-z0-9])

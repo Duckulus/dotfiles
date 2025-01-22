a7cad6c7c4fb3f3f54564b5e8525fe96bcae86b1
Duckulus <amin.haddou@outlook.de>
2025-01-22 14:03:10 +0100

    update waybar

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -15,44 +15,8 @@ killall waybar
 pkill waybar
 sleep 0.5
 
-# ----------------------------------------------------- 
-# Default theme: /THEMEFOLDER;/VARIATION
-# ----------------------------------------------------- 
-themestyle="/ml4w;/ml4w/light"
-
-# ----------------------------------------------------- 
-# Get current theme information from ~/.config/ml4w/settings/waybar-theme.sh
-# ----------------------------------------------------- 
-if [ -f ~/.config/ml4w/settings/waybar-theme.sh ]; then
-    themestyle=$(cat ~/.config/ml4w/settings/waybar-theme.sh)
-else
-    touch ~/.config/ml4w/settings/waybar-theme.sh
-    echo "$themestyle" > ~/.config/ml4w/settings/waybar-theme.sh
-fi
-
-IFS=';' read -ra arrThemes <<< "$themestyle"
-echo ":: Theme: ${arrThemes[0]}"
-
-if [ ! -f ~/.config/waybar/themes${arrThemes[1]}/style.css ]; then
-    themestyle="/ml4w;/ml4w/light"
-fi
-
-# ----------------------------------------------------- 
-# Loading the configuration
-# ----------------------------------------------------- 
-config_file="config"
-style_file="style.css"
-
-# Standard files can be overwritten with an existing config-custom or style-custom.css
-if [ -f ~/.config/waybar/themes${arrThemes[0]}/config-custom ] ;then
-    config_file="config-custom"
-fi
-if [ -f ~/.config/waybar/themes${arrThemes[1]}/style-custom.css ] ;then
-    style_file="style-custom.css"
-fi
-
 # Check if waybar-disabled file exists
 if [ ! -f $HOME/.cache/waybar-disabled ] ;then 
-    waybar -c ~/.config/waybar/themes${arrThemes[0]}/$config_file -s ~/.config/waybar/themes${arrThemes[1]}/$style_file &
+    waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
 fi
 
